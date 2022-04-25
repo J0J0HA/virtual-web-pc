d0640b3d442d7d2b0266ef44b33390f6cb41eb9b
J0J0HA <huhndietlinde@gmail.com>
2022-04-25 15:31:41 +0200

    Updated

---
+++ src/BIOS/util.js
@@ -0,0 +1,20 @@
+var console.output = [];
+
+var _log = console.log,
+    _warn = console.warn,
+    _error = console.error;
+
+console.log = function() {
+    console.output.push({method: 'log', arguments: arguments});
+    return _log.apply(console, arguments);
+};
+
+console.warn = function() {
+    console.output.push({method: 'warn', arguments: arguments});
+    return _warn.apply(console, arguments);
+};
+
+console.error = function() {
+    console.output.push({method: 'error', arguments: arguments});
+    return _error.apply(console, arguments);
+};
