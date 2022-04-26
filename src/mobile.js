5d5bb4454da332dab79ab929a703737c03c6cfd4
J0J0HA <huhndietlinde@gmail.com>
2022-04-26 20:50:18 +0200

    Update mobile.js

--- src/mobile.js
+++ src/mobile.js
@@ -2,13 +2,9 @@ if (window.location.hash == "#mobile") {
   alert("mobile")
   var mobile_console_input = document.createElement('input');
   mobile_console_input.style.display = "none";
-  mobile_console_input.addEventListener('input', function() {
-    cout.current_input = mobile_console_input.value;
-    cout.update();
-    this.focus();
-  })
-  document.body.appendChild(mobile_console_input);
+  document.body.prependChild(mobile_console_input);
   mobile_console_input.focus();
-  document.body.addEventListener("focus", function() {mobile_console_input.focus()})
-  document.body.addEventListener("click", function() {mobile_console_input.focus()})
+  mobile_console_input.click();
+  document.body.addEventListener("focus", function() {mobile_console_input.focus(); mobile_console_input.click()})
+  document.body.addEventListener("click", function() {mobile_console_input.focus(); mobile_console_input.click()})
 }
