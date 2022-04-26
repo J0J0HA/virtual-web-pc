1723423d306c63a78d886eb140f64cf13c8209c9
J0J0HA <huhndietlinde@gmail.com>
2022-04-26 20:58:25 +0200

    Update mobile.js

--- src/mobile.js
+++ src/mobile.js
@@ -2,7 +2,11 @@ if (window.location.hash == "#mobile") {
   alert("mobile")
   var mobile_console_input = document.createElement('input');
   // mobile_console_input.style.display = "none";
-  document.body.prependChild(mobile_console_input);
+  mobile_console_input.addEventListener("onkeydown", function(){
+    this.value = "";
+    cout.log(event.keyCode)
+  })
+  document.body.prepend(mobile_console_input);
   mobile_console_input.focus();
   mobile_console_input.click();
   document.body.addEventListener("focus", function() {mobile_console_input.focus(); mobile_console_input.click()})
