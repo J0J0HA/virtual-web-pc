1ce06639588457f83b5ef1c2db2488612351b560
J0J0HA <huhndietlinde@gmail.com>
2022-04-26 20:13:37 +0200

    Update boot.js

--- src/boot.js
+++ src/boot.js
@@ -1,6 +1,6 @@
 const title = document.getElementsByTagName("title")[0];
 const cout = new CustomConsole(document.getElementById("console-output"), true);
 cout.command_handler = function(cmd) {
-  cout.write("Recived: " + cmd)
+  cout.write("Recived: '" + cmd + "'")
 }
 cout.focus();
